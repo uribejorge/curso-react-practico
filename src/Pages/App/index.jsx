9ee7b4150b6e3fcdbed2d5c2002d4224b7a2e967
jorge uribe <jouribero@cesde.net>
2025-01-12 18:58:28 -0500

    navbar

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -5,6 +5,7 @@ import MyOrder from '../MyOrder';
 import MyOrders from '../MyOrders';
 import NotFound from '../NotFound';
 import SignIn from '../SignIn';
+import Navbar from '../../Components/Navbar'
 import './App.css'
 
 
@@ -27,6 +28,7 @@ const App = () => {
 return (
     <BrowserRouter>
     <AppRoutes />
+    <Navbar />
     </BrowserRouter>
   )
 }
