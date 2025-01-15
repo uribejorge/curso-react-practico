97baeccc120b39d00f8db68ac83c7214bc5c795f
jorge uribe <jouribero@cesde.net>
2025-01-15 18:56:43 -0500

    total-compra

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -1,10 +1,17 @@
 import { useContext } from 'react'
 import { XMarkIcon } from '@heroicons/react/24/solid'
 import { ShoppingCartContext } from '../../Context'
+import OrderCard from '../../Components/OrderCard'
+import { totalPrice } from '../../utils'
 import './styles.css'
 
 const CheckoutSideMenu = () => {
   const context = useContext(ShoppingCartContext)
+
+  const handleDelete = (id) => {
+    const filteredProducts = context.cartProducts.filter(product => product.id != id)
+    context.setCartProducts(filteredProducts)
+  }
 
   return (
     <aside
@@ -16,6 +23,26 @@ const CheckoutSideMenu = () => {
             className='h-6 w-6 text-black cursor-pointer'
             onClick={() => context.closeCheckoutSideMenu()}></XMarkIcon>
         </div>
+      </div>
+      <div className='px-6 overflow-y-scroll'>
+        {
+          context.cartProducts.map(product => (
+            <OrderCard
+              key={product.id}
+              id={product.id}
+              title={product.title}
+              imageUrl={product.images}
+              price={product.price}
+              handleDelete={handleDelete}
+            />
+          ))
+        }
+      </div>
+      <div className='px-6'>
+        <p className='flex justify-between items-center'>
+          <span className='font-light'>Total:</span>
+          <span className='font-medium text-2xl'>${totalPrice(context.cartProducts)}</span>
+        </p>
       </div>
     </aside>
   )
